80374d0334611a39a6197eb215550c08f9cecd89
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-24 15:45:23 +0100

    feat: implement create users

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-console */
 const express = require('express');
 const helmet = require('helmet');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const Users = require('./users/users-model.js');
+require('dotenv').config();
 
 const server = express();
 
@@ -9,6 +13,46 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
+});
+
+const generateToken = (id, department) => {
+  const token = jwt.sign(
+    {
+      userId: id,
+      department,
+    },
+    process.env.SECRET,
+    { expiresIn: '1d' },
+  );
+  return token;
+};
+
+server.post('/api/register', (req, res) => {
+  const { username, password, department } = req.body;
+  if (username && password && department) {
+    const hashedPassword = bcrypt.hashSync(password, 12);
+    const user = {
+      username,
+      password: hashedPassword,
+      department,
+    };
+    Users.add(user)
+      .then((saved) => {
+        const token = generateToken(saved.id, saved.department);
+        res.status(201).json({
+          id: saved.id,
+          username: saved.username,
+          department: saved.department,
+          token,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({ message: 'Required field(s) missing' });
+  }
 });
 
 const port = process.env.PORT || 5000;
